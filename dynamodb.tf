49f12cad60241963ac36c54f47b839d509c17b49
root <root@DESKTOP-8A1PVKI.localdomain>
2021-06-13 09:43:54 +0530

    Adding with patch

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,10 +1,23 @@
 resource "aws_dynamodb_table" "machine_properties" {
   hash_key = "MachineID"
   name = "${local.system_name}-MachineState"
-  attribute {
-    name = "MachineID"
-    type = "S"
-  }
-
   billing_mode = "PAY_PER_REQUEST"
+  attribute {
+     name = "MachineID"
+     type = "S"
+   }
+}
+
+
+resource "aws_dynamodb_table_item" "machine_properties" {
+  table_name = aws_dynamodb_table.machine_properties.name
+  hash_key   = aws_dynamodb_table.machine_properties.hash_key
+
+  item = <<ITEM
+{
+  "MachineID": {"S": "i-0eeabf6aab5e3e4ed"},
+  "AvailabilityZone": {"S": "us-east-1"},
+  "InstanceType": {"S": "t3.micro" }
+}
+ITEM
 }
